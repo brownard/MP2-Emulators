aded6f67857894df2a296e720132d81db5036cbf
brownard <brownard@live.co.uk>
2021-04-16 11:31:26 +0100

    Add game media lists

---
+++ Emulators/Emulators/MediaExtensions/MediaLists/FavoriteGameMediaListProvider.cs
@@ -0,0 +1,23 @@
+﻿using Emulators.Common.Games;
+using Emulators.Game;
+using Emulators.Models.Navigation;
+using MediaPortal.Common;
+using MediaPortal.Common.Commands;
+using MediaPortal.UiComponents.Media.MediaLists;
+
+namespace Emulators.MediaExtensions.MediaLists
+{
+  public class FavoriteGameMediaListProvider : BaseFavoriteMediaListProvider
+  {
+    public FavoriteGameMediaListProvider()
+    {
+      _changeAspectId = GameAspect.ASPECT_ID;
+      _necessaryMias = EmulatorsConsts.NECESSARY_GAME_MIAS;
+      _playableConverterAction = item => new GameItem(item)
+      {
+        Command = new MethodDelegateCommand(() => ServiceRegistration.Get<IGameLauncher>().LaunchGame(item))
+      };
+      _navigationInitializerType = typeof(GamesLibrary);
+    }
+  }
+}
---
+++ Emulators/Emulators/MediaExtensions/MediaLists/LatestGameMediaListProvider.cs
@@ -0,0 +1,21 @@
+﻿using Emulators.Game;
+using Emulators.Models.Navigation;
+using MediaPortal.Common;
+using MediaPortal.Common.Commands;
+using MediaPortal.UiComponents.Media.MediaLists;
+
+namespace Emulators.MediaExtensions.MediaLists
+{
+  public class LatestGameMediaListProvider : BaseLatestMediaListProvider
+  {
+    public LatestGameMediaListProvider()
+    {
+      _necessaryMias = EmulatorsConsts.NECESSARY_GAME_MIAS;
+      _playableConverterAction = item => new GameItem(item)
+      {
+        Command = new MethodDelegateCommand(() => ServiceRegistration.Get<IGameLauncher>().LaunchGame(item))
+      };
+      _navigationInitializerType = typeof(GamesLibrary);
+    }
+  }
+}
